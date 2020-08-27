3acc90c36cd6eeaa20a1c89133a5fedc5fbf58bb
Deba199 <70008841+Deba199@users.noreply.github.com>
2020-08-27 14:06:17 +0530

    Update firstpython.py

--- firstpython.py
+++ firstpython.py
@@ -1,2 +1,11 @@
 # Display the output
 print("New python file")
+class car:
+  def __init__(self,a=40'):
+     self._speed=a
+  def get_speed(self):
+     return self._speed
+  def set_speed(self,a):
+      self._speed=a
+      return
+  speed=property(get_speed,set_speed)
